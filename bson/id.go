fb7f21f051fb59880d174b06761cf75e264445c6
Your Name <you@example.com>
2020-12-31 10:29:33 -0500

    adding in our bsonid that is compatible with mongo and features some unique features

---
+++ bson/id.go
@@ -0,0 +1,87 @@
+package id
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"sync/atomic"
+	"time"
+)
+
+// ObjectID is the BSON ObjectID type.
+type ObjectID [12]byte
+
+var objectIDCounter = readRandomUint32()
+var processUnique = processUniqueBytes()
+
+func init() {
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			objectIDCounter = readRandomUint32()
+		}
+	}()
+}
+
+func NewID() string {
+	return NewObjectID().Hex()
+}
+
+// newObjectID generates a new ObjectID.
+func NewObjectID() ObjectID {
+	return newObjectIDFromTimestamp(time.Now())
+}
+
+// newObjectIDFromTimestamp generates a new ObjectID based on the given time.
+func newObjectIDFromTimestamp(timestamp time.Time) ObjectID {
+	var b [12]byte
+
+	binary.BigEndian.PutUint32(b[0:4], uint32(timestamp.Unix()))
+	copy(b[4:9], processUnique[:])
+	putUint24(b[9:12], atomic.AddUint32(&objectIDCounter, 1))
+
+	return b
+}
+
+// Timestamp extracts the time part of the ObjectId.
+func (id ObjectID) Timestamp() time.Time {
+	unixSecs := binary.BigEndian.Uint32(id[0:4])
+	return time.Unix(int64(unixSecs), 0).UTC()
+}
+
+// Hex returns the hex encoding of the ObjectID as a string.
+func (id ObjectID) Hex() string {
+	return hex.EncodeToString(id[:])
+}
+
+func (id ObjectID) String() string {
+	return fmt.Sprintf("ObjectID(%q)", id.Hex())
+}
+
+func processUniqueBytes() [5]byte {
+	var b [5]byte
+	_, err := io.ReadFull(rand.Reader, b[:])
+	if err != nil {
+		panic(fmt.Errorf("cannot initialize objectid package with crypto.rand.Reader: %v", err))
+	}
+
+	return b
+}
+
+func readRandomUint32() uint32 {
+	var b [4]byte
+	_, err := io.ReadFull(rand.Reader, b[:])
+	if err != nil {
+		panic(fmt.Errorf("cannot initialize objectid package with crypto.rand.Reader: %v", err))
+	}
+
+	return (uint32(b[0]) << 0) | (uint32(b[1]) << 8) | (uint32(b[2]) << 16) | (uint32(b[3]) << 24)
+}
+
+func putUint24(b []byte, v uint32) {
+	b[0] = byte(v >> 16)
+	b[1] = byte(v >> 8)
+	b[2] = byte(v)
+}
