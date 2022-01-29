46d428075ba7cc41f2daac922ce8af2f10faaa18
Your Name <you@example.com>
2022-01-29 03:07:13 -0800

    added comparisons and generation and conversion to base32 to the simple example since the old example code is now irrelevant

--- cmd/example/simple/main.go
+++ cmd/example/simple/main.go
@@ -9,15 +9,30 @@ import (
 func main() {
 	fmt.Println("multiverse sortable time stamped id")
 	fmt.Println("============================================================")
-	for i := 1; i <= 2; i++ {
-		//fmt.Println("Id.New().String():", id.New().String())
-		//fmt.Println("Id.New().Bytes():", id.New().Bytes())
-		//fmt.Println("Id.New().Nonce():", id.New().Nonce())
-		//fmt.Println("Id.New().Pid():", id.New().Pid())
-		//fmt.Println("Id.New().Time():", id.New().Time())
-		//fmt.Println("Id.New().Short():", id.New().Short())
-		//fmt.Println("Id.New().NoPrefix():", id.New().NoPrefix())
-		//v, _ := id.New().Value()
-		//fmt.Println("Id.New().Value():", v)
-	}
+
+  id1 := id.Generate()
+  id2 := id.Generate()
+
+  fmt.Println("id 1 is: ", id1)
+  fmt.Println("id 2 is: ", id2)
+
+  fmt.Println("now testing comparison")
+
+  fmt.Println(" id1.Compare(id1) = ", id1.Compare(id1))
+  fmt.Println(" id1.Compare(id2) = ", id1.Compare(id2))
+  fmt.Println(" id1.Compare(id1) = ", id2.Compare(id1))
+  fmt.Println(" id2.Compare(id2) = ", id2.Compare(id2))
+
+  fmt.Println("now converting to base32 and comparing")
+
+  id1 = id1.Base32()
+  id2 = id2.Base32()
+
+  fmt.Println("id (base32) 1 is: ", id1)
+  fmt.Println("id (base32) 2 is: ", id2)
+
+  fmt.Println(" id1.Compare(id1) = ", id1.Compare(id1))
+  fmt.Println(" id1.Compare(id2) = ", id1.Compare(id2))
+  fmt.Println(" id2.Compare(id1) = ", id2.Compare(id1))
+  fmt.Println(" id2.Compare(id2) = ", id2.Compare(id2))
 }
