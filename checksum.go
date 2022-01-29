8a2eb4ac204a6fb5a74762690410fe98863cee10
Your Name <you@example.com>
2022-01-29 01:46:40 -0800

    completed the validate functions in checksum.go

--- checksum.go
+++ checksum.go
@@ -6,8 +6,6 @@ import (
   "hash/adler32"
 )
 
-// NOTE: Because we use time, the values can never be all zero,
-//       so this method will work reliably. 
 func simpleChecksumByte(idBytes []byte) byte {
   var sum uint8
   for _, idByte := range idBytes {
@@ -16,14 +14,26 @@ func simpleChecksumByte(idBytes []byte) byte {
   return byte(sum / uint8(len(idBytes)))
 }
 
+func simpleChecksumValid(idBytes []byte, checksumByte byte) bool {
+  return simpleChecksumByte(idBytes) == checksumByte
+}
+
 func crc32ChecksumBytes(id Id) []byte {
   byteBuffer := make([]byte, 4)
   binary.BigEndian.PutUint32(byteBuffer, crc32.ChecksumIEEE(id))
   return byteBuffer
 }
 
+func crc32ChecksumValid(id Id, checksumBytes []byte) bool {
+  return crc32ChecksumBytes(id) == checksumBytes
+}
+
 func adler32ChecksumBytes(id Id) []byte {
   byteBuffer := make([]byte, 4)
   binary.BigEndian.PutUint32(byteBuffer, adler32.Checksum(id))
   return byteBuffer 
+}
+
+func adler32ChecksumValid(id Id, checksumBytes []byte) bool {
+  return adler32ChecksumBytes(id) == checksumBytes
 }
