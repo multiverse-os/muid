31b9e003328dae779c6655b4ba22ad5c9f9fb669
Your Name <you@example.com>
2022-01-31 16:02:48 -0800

    filling out basic functionality with the aim to completely use every feature of the library making it a foundational tool for both testing and reference

--- cmd/muid/main.go
+++ cmd/muid/main.go
@@ -16,10 +16,40 @@ func main() {
     Description: "A command-line tool for generating muid keys.",
     GlobalFlags: []cli.Flag{
       cli.Flag{
-        Name:        "randbytes",
+        Name:        "random",
         Alias:       "r",
         Default:     "3",
         Description: "Specify number of random bytes to use",
+      },
+      cli.Flag{
+        Name:        "append",
+        Alias:       "a",
+        Default:     "",
+        Description: "Append text as prefix or suffix to the `muid`",
+      },
+      cli.Flag{
+        Name:        "time",
+        Alias:       "t",
+        Default:     "now",
+        Description: "Time used to seed the `muid`",
+      },
+      cli.Flag{
+        Name:        "checksum",
+        Alias:       "c",
+        Default:     "simple",
+        Description: "Checksum type used in `muid`",
+      },
+      cli.Flag{
+        Name:        "hash",
+        Alias:       "h",
+        Default:     "",
+        Description: "Two-way hash to conceal information stored within `muid`",
+      },
+      cli.Flag{
+        Name:        "compress",
+        Alias:       "x",
+        Default:     "",
+        Description: "Compression algorithm used on the `muid`",
       },
     },
   })
